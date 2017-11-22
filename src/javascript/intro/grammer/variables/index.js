d34e1221fc93575dead513ddb7e6f85e5e20de73
mistychris6 <mistychris6@gmail.com>
2017-11-21 18:01:58 -0700

    Completed

---
+++ src/javascript/intro/grammer/variables/index.js
@@ -0,0 +1,16 @@
+export var message = 'hello';
+
+export let greeting = 'Yo!';
+
+export const saying = 'What up';
+
+export var message = 'world';
+
+export let greeting = 'Hello!';
+
+export const saying = 'Booyah';
+
+console.log (message);
+console.log (greeting);
+console.log (saying);
+
